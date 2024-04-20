8dba707f62b78205c327cf39f8c3ba76f9c51128
Anthony <46854057+MasterPlayerOfficial@users.noreply.github.com>
2024-04-19 21:30:39 -0400

    Added addAlert

--- app/Http/Controllers/AlertController.php
+++ app/Http/Controllers/AlertController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+
+use App\Models\Alert;
+
+use Carbon\Carbon;
 
 class AlertController extends Controller
 {
-    //
+    public function addAlert(Request $request)
+    {
+        try
+        {
+            $alert = new Alert([
+                'idAnimal' => $request->input('idAnimal'),
+                'inProgress' => $request->input('inProgress'),
+                'dateStart' => Carbon::now()->format('Y-m-d'),
+                'dateEnd' => $request->input('dateEnd')
+            ]);
+
+            $alert->save();
+
+            return response()->json(true);
+        }
+        catch(QueryException $e)
+        {
+            return response()->json(['Message' => 'An error has occured: ' . $e->getMessage()], 500);
+        }
+    }
 }
