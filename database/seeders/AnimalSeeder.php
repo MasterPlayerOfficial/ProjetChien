ade3f7994275bce8f2115a89bd0b138f46a58c69
Anthony <46854057+MasterPlayerOfficial@users.noreply.github.com>
2024-05-12 17:03:29 -0400

    Properly added AnimalSeeder

--- database/seeders/AnimalSeeder.php
+++ database/seeders/AnimalSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
 
 class AnimalSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class AnimalSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('animal')->insert([
+            'name' => Str::random(10),
+            'birth' => Date::random(),
+            'race' => Str::random(10),
+            'color' => Str::random(10),
+            'lost' => 0
+        ]);
     }
 }
