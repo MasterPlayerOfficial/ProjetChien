e2946f8aaa88b46801b2c69794ede701681557d9
Anthony <46854057+MasterPlayerOfficial@users.noreply.github.com>
2024-05-15 12:09:10 -0400

    Typo

--- app/Http/Controllers/AlertController.php
+++ app/Http/Controllers/AlertController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
-use PhpWqtt\Client\Facades\MQTT;
+use PhpMqtt\Client\Facades\MQTT;
 
 use App\Models\Alert;
 
