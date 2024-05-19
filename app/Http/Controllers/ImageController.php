f9f452935e0664d4744b636a20ce83592829dab1
Anthony <46854057+MasterPlayerOfficial@users.noreply.github.com>
2024-05-19 17:28:12 -0400

    Début test images

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Log;
+
 class ImageController extends Controller
 {
     public function uploadImageToServer(Request $request)
     {
         $imageData = $request->input('image_data');
+        Log::debug($imageData);
         $imageExtracted = json_decode($imageData);
         $nameOfImage = 'image_' . time() . '.png';
         file_put_contents(public_path('img/' . $nameOfImage), $imageExtracted);
